657e5444c2d0f0fe241c1c973d23e83883652b11
AipBorges <mpbds@uic.es>
2021-11-05 16:42:16 +0100

    first commit ever in life

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+    "lines-between-class-members": "off",
+    "class-methods-use this": "off",
+    "import/extensions": "off",
+  },
+};
