9b8f35625fee25eef6b80af160ebad8d658b9bbd
AipBorges <mpbds@uic.es>
2021-11-05 21:18:34 +0100

    id robots get done.

--- server/routes/robotsRoutes.js
+++ server/routes/robotsRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const getRobots = require("./controller/robotsController");
+const { getRobots, getARobot } = require("./controller/robotsController");
 
 const router = express.Router();
 
 router.get("/", getRobots);
+router.get("/:idRobot", getARobot);
 
 module.exports = router;
